1fbbc0f1f76e3b23772591b1773dce58503f9489
Pere Villega <pere.villega@barclayscapital.com>
2015-10-20 21:58:13 +0100

    Add new exercise

---
+++ src/main/scala/com/perevillega/ch9/Exercises.scala
@@ -0,0 +1,2 @@
+package com.perevillega.ch9
+
---
+++ src/test/scala/com/perevillega/ch9/ExercisesSpec.scala
@@ -0,0 +1,9 @@
+package com.perevillega.ch9
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class ExercisesSpec extends FlatSpec with Matchers {
+
+  "Ch9" should "run the first test example" in {
+  }
+}
