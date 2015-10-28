14c1ecd784ac9699bc230fbfb3ff4029e97dd7f7
Pere Villega <pere.villega@barclayscapital.com>
2015-10-28 23:18:59 +0000

    Add code for chapter 9 (WIP)

--- src/main/scala/com/perevillega/ch9/Exercises.scala
+++ src/main/scala/com/perevillega/ch9/Exercises.scala
@@ -1,2 +1,29 @@
 package com.perevillega.ch9
 
+import scala.language.implicitConversions
+
+trait Parsers[ParseError, Parser[+ _]] {
+  self =>
+
+  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+
+  implicit def char(c: Char): Parser[Char]
+
+  implicit def string(s: String): Parser[String]
+
+  implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
+
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+
+  case class ParserOps[A](p: Parser[A]) {
+    def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+
+    def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+  }
+
+}
+
